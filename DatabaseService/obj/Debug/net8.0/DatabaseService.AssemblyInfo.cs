cfad7f2fdd07ac0cec95c29099a3a9848465ff90
Thet Wai Aung <twa453354@gmail.com>
2024-05-26 14:34:20 +0630

    commit ddl files

--- DatabaseService/obj/Debug/net8.0/DatabaseService.AssemblyInfo.cs
+++ DatabaseService/obj/Debug/net8.0/DatabaseService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f36604b0c07c09cd64cfe800714f2f866546a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31cb525720afe7f7cdd9e425a22f320311908a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
